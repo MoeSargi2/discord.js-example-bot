59a56e600562b5ed9dcd6eb06cff96da486aa5d4
Leo Berardi <51487047+MoeSargi2@users.noreply.github.com>
2019-10-13 13:04:44 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var bot = new Discord.Client();
 
 // Events.
 bot.on("ready", function() {
-    bot.user.setGame(`Hi, Im New Bot On Discord!`);
+    bot.user.setGame(`Checking wish accs with money`);
     console.log(`${bot.user.username} is Ready!`);
 });
 
